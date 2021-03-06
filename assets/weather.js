a867bed9ef4688049858420bff169f2d508865e6
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-06 13:40:27 -0500

    adding weather information acquisition

--- assets/weather.js
+++ assets/weather.js
@@ -1,14 +1,28 @@
-const 
+const weatherDiv = $('#weatherContainer');
+const weatherInfo = $('#weatherInfo');
+const tempSpot = $('#temp');
+const feelsLike = $('#feelsLike');
 
+
+
+// https://openweathermap.org/current
 function getWeather() {
     let weatherUrl = 'http://api.openweathermap.org/data/2.5/find?q=Columbus&units=imperial&appid=096317ae116f5805e156e4177ebd6d5a';
 
     fetch(weatherUrl)
-    .then(function(response) {
+    .then(function (response) {
         return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
         //add to page here
+        tempSpot.text(Math.floor(data.list[0].main.temp));
+        feelsLike.text(Math.floor(data.list[0].main.feels_like));
+        $('#icon').attr('src', `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`);
+        $('#windSpeed').text(Math.floor(data.list[0].wind.speed));
+        console.log(data);
         console.log(data.list[0].main.temp);
+        console.log(data.list[0].weather[0].icon);
     })
 }
+
+getWeather();
