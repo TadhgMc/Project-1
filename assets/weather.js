914871d09e67be0796abcbbc980bb51e2100ac30
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-09 21:10:13 -0500

    cleaned code

--- assets/weather.js
+++ assets/weather.js
@@ -3,9 +3,7 @@ const weatherInfo = $('#weatherInfo');
 const tempSpot = $('#temp');
 const feelsLike = $('#feelsLike');
 
-
-
-// https://openweathermap.org/current
+// fetch request to get weather data
 function getWeather() {
     let weatherUrl = 'http://api.openweathermap.org/data/2.5/find?q=Columbus&units=imperial&appid=096317ae116f5805e156e4177ebd6d5a';
 
@@ -20,10 +18,6 @@ function getWeather() {
         $('#icon').attr('src', `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`);
         $('#wConditions').text(data.list[0].weather[0].description);
         $('#windSpeed').text(Math.floor(data.list[0].wind.speed));
-        //if to check for rain or snow, if null say none, if something->say what it is
-        console.log(data);
-        console.log(data.list[0].main.temp);
-        console.log(data.list[0].weather[0].icon);
     })
 }
 
