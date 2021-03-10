56aaff34419eab8407d4e8cc30fbe7309410eeff
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-09 21:14:24 -0500

    added s to http links so that they will work in github pages

--- assets/weather.js
+++ assets/weather.js
@@ -5,7 +5,7 @@ const feelsLike = $('#feelsLike');
 
 // fetch request to get weather data
 function getWeather() {
-    let weatherUrl = 'http://api.openweathermap.org/data/2.5/find?q=Columbus&units=imperial&appid=096317ae116f5805e156e4177ebd6d5a';
+    let weatherUrl = 'https://api.openweathermap.org/data/2.5/find?q=Columbus&units=imperial&appid=096317ae116f5805e156e4177ebd6d5a';
 
     fetch(weatherUrl)
     .then(function (response) {
@@ -15,7 +15,7 @@ function getWeather() {
         //add to page here
         tempSpot.text(Math.floor(data.list[0].main.temp));
         feelsLike.text(Math.floor(data.list[0].main.feels_like));
-        $('#icon').attr('src', `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`);
+        $('#icon').attr('src', `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`);
         $('#wConditions').text(data.list[0].weather[0].description);
         $('#windSpeed').text(Math.floor(data.list[0].wind.speed));
     })
