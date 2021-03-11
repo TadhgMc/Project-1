ce59bb866f3ba284337468a938d0b54894e83f01
Edita P <blugirlnyc@hotmail.com>
2021-03-11 16:35:12 -0500

    change to function getLocalEvents

--- assets/localevents.js
+++ assets/localevents.js
@@ -1,5 +1,8 @@
+const localEventsDiv = $('#localActivitiesContainer');
+const localEventsInfo = $('#localActivities');
 var tmApiKey = '3uaJfxHrGuBFeFN7pSjJEALAzNssmWGc';
 
+//check if city=columbus in url is correct & works
 function getLocalEvents() {
     let localEventsUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?city=columbus&apikey=3uaJfxHrGuBFeFN7pSjJEALAzNssmWGc';
 
