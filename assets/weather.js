4c265eee36ecf06adfec7c5d44ee69b137a0e792
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-03-12 11:51:31 -0500

    working through a funstion that adds forecast info to the page for selected number of days

--- assets/weather.js
+++ assets/weather.js
@@ -37,12 +37,20 @@ $('#foreBtn').on('click', function(event){
     .then( function (data) {
         //taking data from weather api and putting it onto the page
         console.log(data);
-
+        let foreListItem = 7;
         const foreDays = $('#numOfFore').val();
         console.log(foreDays);
+        $('div.forecastContainer').html("<div class='forecastCards'></div>");
         for (i=0; i < foreDays ; i++) {
             console.log("test");
-            
+            //get data first, then store in variables that are what are added to the elements that are created
+            let foreIcon = $("<img>").attr('src', `https://openweathermap.org/img/wn/${data.list[foreListItem].weather[0].icon}@2x.png`);
+            let foreDate = $("<p class='forecastDate'><p>").text(moment(data.list[foreListItem].dt_txt).format('dddd, M/D/YYYY'));
+            let foreTemp = $("<p class='foreTemp'>&#8457<p>").prepend(Math.floor(data.list[foreListItem].main.feels_like));
+            let foreConditions = $("<p class='foreCond'>Forecasted Conditions: <p>").append(data.list[foreListItem].weather[0].description);
+
+            $('.forecastCards').after(`<div class='day${i}'></div>`)
+            foreListItem = foreListItem + 7; //this needs to be at the end
         }
         
     });
